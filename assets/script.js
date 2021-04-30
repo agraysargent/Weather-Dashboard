b559438423bf0208173d2a286b15471d5dfdc83a
Angelica Sargent <agraysargent@gmail.com>
2021-04-30 12:25:37 -0400

    edited script errors

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 let city = $("#searchTerm").val();
 const apiKey = "&appid=afaa8eea1769b4359fd8e07b2efcefbd";
 let date = new Date();
+const cities = [""]; 
+
+localStorage.setItem("cities", JSON.stringify(cities))
 
 // function & event enabled for user to search terms, also prevent default enabled to prevent page refresh
 $("#searchTerm").keypress(function (event) {
@@ -14,11 +17,14 @@ $("#searchTerm").keypress(function (event) {
 $("#searchBtn").on("click", function () {
   $("#forecastH5").addClass("show");
   city = $("#searchTerm").val();
-  $("#searchTerm").val("");
-
+ let cityHistory = JSON.parse(localStorage.getItem("cities"));
+ let cityHistory2 = localStorage.getItem("cities");
+ cityHistory.push(city)
+ console.log(cityHistory2, 'this is a string')
+  //localStorage.setItem(city, city);
   // website pulling the api data
   const queryURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
+  "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
 
   $.ajax({
     url: queryURL,
@@ -26,12 +32,12 @@ $("#searchBtn").on("click", function () {
   }).then(function (response) {
     console.log(response);
     // googled math.floor via stackoverflow for assistance on development below
-    let tempF = (response.amin.temp - 273.15) * 1.8 + 32;
+    let tempF = (response.list[0].main.temp - 273.15) * 1.8 + 32;
     console.log(Math.floor(tempF));
 
-    getCurrentConditions(response);
+    // getCurrentConditions(response);
     getCurrentForecast(response);
-    makeList();
+    // makeList();
   });
 });
 
@@ -75,40 +81,47 @@ function getCurrentConditions(response) {
   $("#currentCity").append(card);
 }
 
-function getCurrentForecast () {
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
-    console.log(response.dt)
-    $('#forecast').empty();
+function getCurrentForecast(results) {
+  // $.ajax({
+  //     url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+  //     method: "GET"
+  // }).then(function(response){
+  //     console.log(response)
+  //     console.log(response.dt)
+  //     $('#forecast').empty();
 
-    let results = response.list;
-    console.log(results)
+  //     let results = response.list;
+  //     console.log(results)
+  console.log("hello");
+  for (let i = 0; i < results.list.length; i++) {
+    let day = Number(results.list[i].dt_txt.split("-")[2].split("")[0]);
+    let hour = results.list[i].dt_txt.split("-")[2].split(" ")[1];
+    // console.log(day);
+    // console.log({hour}, typeof hour);
+    let key = results.list[i];
+    // console.log(key)
+    //  console.log(hour == "12:00:00")
+    if (hour == "12:00:00") {
+      console.log("it's the hour ");
+      //   console.log('hour')
+      let temp = (results.list[i].main.temp - 273.15) * 1.8 + 32;
+      let tempF = Math.floor(temp);
+      // console.log(tempF)
 
-    for (let i = 0; i < results.length; i++){
-        let day = Number(results[i].dt_txt.split('-')[2].split('')[0]);
-        let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
-        console.log(day);
-        console.log(hour);
+          const card = $("<div>").addClass("card col-md ml-4 bg-primary text-white");
+          console.log(card)
+          const cardBody = $("<div>").addClass("card-body p-3 forecastBody");
+          const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+          const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF +  " °F");
+          const humidity =  $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results.list[i].main.humidity + "%");
+          const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results.list[i].weather[0].icon + ".png")
 
-        if(results[i].dt_txt.indexOf("12:00:00") !== -1){
-            let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
-            let tempF = Math.floor(temp);
-
-            const card = $("<div>").addClass("card col-md ml-4 bg-primary text-white");
-            const cardBody = $("<div>").addClass("card-body p-3 forecastBody");
-            const cityDate = $("<h4").addClass("card-title").text(date.toLocaleDateString('en-US'));
-            const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF +  " °F");
-            const humdity =  $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
-            const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
-
-            cardBody.append(cityDate, image, temperature, humidity);
-            card.append(cardBody);
-            $("#forecast").append(card);
-        }
+          cardBody.append(cityDate, image, temperature, humidity);
+        console.log(card)
+          card.append(cardBody);
+          console.log(card)
+          $("#forecast").append(card);
     }
-});
-
+  }
+  // });
 }
